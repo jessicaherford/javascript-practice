0ecfed10458a12399a2c044cf92c62c386b9f2c2
JessicaHerford <jrherford12@gmail.com>
2016-05-28 07:39:38 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Javascript Practice
+
+Hi there!
+
+Welcome to my Code Wars practice repo.
---
+++ sumofpairs.js
@@ -0,0 +1,13 @@
+var sum_pairs = function(ints, s){
+    ints.forEach(function(int){
+      console.log(int);
+    })
+    console.log('******' + s + '******');
+}
+
+
+l1 = [1, 4, 8, 7, 3, 15];
+
+// 7 + 1
+
+sum_pairs(l1, 8);
